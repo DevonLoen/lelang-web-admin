bd0c28d16e84f2cd9aafb2476d74982b50698869
DevonLoen <devonloen99@gmail.com>
2025-09-29 09:28:11 +0700

    feat(product): add admin management product pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,34 @@ import Login from "./pages/Auth/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import AdminLayout from "./layout/AdminLayout";
 import CreateAdmin from "./pages/Auth/CreateAdmin";
+import Product from "./pages/product/product";
+import PendingProduct from "./pages/product/pending-product";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   const token = localStorage.getItem("token");
 
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-      {/* Group route untuk admin yang pakai Sidebar */}
-      <Route
-        path="/admin"
-        element={token ? <AdminLayout /> : <Navigate to="/login" />}
-      >
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="create-admin" element={<CreateAdmin />} />
-      </Route>
+        {/* Group route untuk admin yang pakai Sidebar */}
+        <Route
+          path="/admin"
+          element={token ? <AdminLayout /> : <Navigate to="/login" />}
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="create-admin" element={<CreateAdmin />} />
+          <Route path="product" element={<Product />} />
+          <Route path="product/pending" element={<PendingProduct />} />
+        </Route>
 
-      {/* default route */}
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+        {/* default route */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </>
   );
 }
 
